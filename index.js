0d09648a175c9370fb4bad2d2f4f2f4be5d05ab8
Yonatan <yorpeli@gmail.com>
2019-09-07 12:37:06 +0300

    Before the hub

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
+require('./Services/passport');
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send({
-        hi: 'there',
-        name:'Yonatan'});
-});
+require('./Routes/authRoutes')(app);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
